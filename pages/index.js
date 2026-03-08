516da771493986886f40c919e602709eb82e135f
Alain Iranzi <alainiranzi00@gmail.com>
2026-03-08 15:28:18 +0200

    a closer look at get static props and configuration options

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
+import { notFound } from 'next/navigation';
 
 function HomePage(props) {
   const { products } = props;
@@ -12,18 +13,24 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('(Re)Generating...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
 
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
 
+  if (data.products.length === 0) {
+    return {
+      notFound: true
+    };
+  }
+
   return {
     props: {
       products: data.products,
     },
-    revalidate: 10,
+    revalidate: 10
   };
 }
 
